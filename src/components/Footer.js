e850c5a592654aa8f9996bc2594f338bb6d86aae
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-05-22 13:33:37 -0700

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer>
+    <p>
+      <a href='https://github.com/lunaxlam/luna-lam-hello-ai-app' 
+        target='blank'>Hello AI by Luna Lam</a> |
+      <a href='https://openai.com/api/' target='blank'>Powered by OpenAI API</a>
+    </p>
+  </footer>
+  )
+}
+
+export default Footer
